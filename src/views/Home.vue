62b9ad5cd53649a6fc23fa4172e76fa35b9445ef
Stratos-Chen <twinschen55@gmail.com>
2020-09-08 13:48:02 -0700

    added indexProfessors method and showcase the index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <article v-for="professor in professors">
+      <h2>{{ professor.name }}</h2>
+      <p>{{ professor.title }}</p>
+      <p>{{ professor.school }}</p>
+      <p>{{ professor.department }}</p>
+      <p>{{ professor.subject }}</p>
+    </article>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
   data: function() {
     return {
-      message: "Welcome to rate my professor!"
+      message: "Welcome to rate my professor!",
+      professors: []
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    this.indexProfessors();
+  },
+  methods: {
+    indexProfessors: function() {
+      axios.get("/professors").then(response => {
+        console.log("All Professors:", response.data);
+        this.professors = response.data;
+      });
+    }
+  }
 };
 </script>
