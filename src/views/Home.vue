9292e1259c52bf5857da39b0755e9480e1c1e94e
Stratos-Chen <twinschen55@gmail.com>
2020-09-08 13:14:49 -0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data: function() {
+    return {
+      message: "Welcome to rate my professor!"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
