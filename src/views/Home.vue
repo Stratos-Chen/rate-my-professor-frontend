8393945d4e6bba4102f4b41af87c7292a74e9cf2
hubert <huby40@gmail.com>
2020-09-08 16:51:20 -0400

    adds review index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div class="review-index" v-for="review in reviews">
+      <p>{{ review.id }}</p>
+      <h4>{{ review.author }}</h4>
+      <p>{{ review.date }}</p>
+      <p>{{ review.text }}</p>
+      <p>{{ review.score }}</p>
+      <p>{{ review.professor_id }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
-  data: function() {
+  data: function () {
     return {
-      message: "Welcome to rate my professor! This is a change."
+      message: "Welcome to rate my professor! This is a change.",
+      reviews: [],
     };
   },
-  created: function() {},
-  methods: {}
+  created: function () {
+    axios.get("/reviews").then((response) => {
+      console.log("All Reviews", response.data);
+      this.reviews = response.data;
+    });
+  },
+  methods: {},
 };
 </script>
