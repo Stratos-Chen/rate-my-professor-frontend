9ecc396d49089fc940a5b1081b46f216818167a3
Stratos-Chen <twinschen55@gmail.com>
2020-09-08 14:45:47 -0700

    added createProfessor method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <article v-for="professor in professors">
+    <div class="professor-index" v-for="professor in professors">
       <h2>{{ professor.name }}</h2>
       <p>{{ professor.title }}</p>
       <p>{{ professor.school }}</p>
       <p>{{ professor.department }}</p>
       <p>{{ professor.subject }}</p>
-    </article>
+    </div>
+    <form class="professor-create" v-on:submit.prevent="createProfessor()">
+      <h2>New Professor</h2>
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>Professor's Name: </label>
+        <input type="text" class="form-control" v-model="name" />
+      </div>
+      <div class="form-group">
+        <label>Title: </label>
+        <input type="text" class="form-control" v-model="title" />
+      </div>
+      <div class="form-group">
+        <label>School: </label>
+        <input type="text" class="form-control" v-model="school" />
+      </div>
+      <div>
+        <label>Department: </label>
+        <input type=text class="form-control" v-model="department" />
+      </div>
+      <div>
+        <label>Subject: </label>
+        <input type=text class="form-control" v-model="subject" />
+      </div>
+      <input type="submit" class="btn btn-primary" value="Create" />
+    </form>
     <div class="review-index" v-for="review in reviews">
       <p>{{ review.id }}</p>
       <h4>{{ review.author }}</h4>
@@ -26,23 +53,47 @@ import axios from "axios";
 
 export default {
   name: "Home",
-  data: function () {
+  data: function() {
     return {
       message: "Welcome to rate my professor! This is a change.",
       professors: [],
       reviews: [],
+      name: "",
+      title: "",
+      school: "",
+      department: "",
+      subject: ""
     };
   },
-  created: function () {
-    axios.get("/professors").then((response) => {
+  created: function() {
+    axios.get("/professors").then(response => {
       console.log("All Professors:", response.data);
       this.professors = response.data;
     });
-    axios.get("/reviews").then((response) => {
+    axios.get("/reviews").then(response => {
       console.log("All Reviews", response.data);
       this.reviews = response.data;
     });
   },
-  methods: {},
+  methods: {
+    createProfessor: function() {
+      var params = {
+        name: this.name,
+        title: this.title,
+        school: this.school,
+        department: this.department,
+        subject: this.subject
+      };
+      axios
+        .post("professors", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.$router.push("/professors");
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
 };
 </script>
