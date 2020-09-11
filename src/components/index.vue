848ba91ae7f5390a3515fc1f3c8f176be40fe16e
hubert <huby40@gmail.com>
2020-09-10 21:36:34 -0400

    adds vue-moment and fixes date format

--- src/components/index.vue
+++ src/components/index.vue
@@ -68,7 +68,7 @@
     <!-- Review Index -->
     <div class="review-index" v-for="review in reviews">
       <h4>Author: {{ review.author }}</h4>
-      <p>Date: {{ review.date }}</p>
+      <p>Date: {{ review.date | moment('MMMM Do YYYY') }}</p>
       <p>Review: {{ review.text }}</p>
       <p>Score: {{ review.score }}</p>
       <p>Professor:{{ getProfessorName(review.professor_id) }}</p>
@@ -102,6 +102,8 @@
 // @ is an alias to /src
 import HelloWorld from "../components/HelloWorld.vue";
 import axios from "axios";
+// import * as moment from "moment/moment";
+import VueMoment from "vue-moment";
 
 export default {
   name: "index",
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import VueMoment from "vue-moment";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMoment);
+
 
 new Vue({
   router,
