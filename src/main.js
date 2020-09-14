a67f2282984c4f6b94913fe1622d5a822bc1314c
Stratos-Chen <twinschen55@gmail.com>
2020-09-14 09:52:43 -0700

    added professorIndex and table

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import VueMoment from "vue-moment";
-import Vue2Filters from 'vue2-filters';
-import StarRating from 'vue-star-rating'
-
-
+import Vue2Filters from "vue2-filters";
+import StarRating from "vue-star-rating";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
@@ -15,10 +13,7 @@ Vue.config.productionTip = false;
 
 Vue.use(VueMoment);
 Vue.use(Vue2Filters);
-Vue.component('star-rating', StarRating);
-
-
-
+Vue.component("star-rating", StarRating);
 
 new Vue({
   router,
---
+++ src/views/ProfessorIndex.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Professor Index">
+    <table>
+      <tr>
+        <td>Professor's Name</td>
+        <td>Title</td>
+        <td>School</td>
+        <td>Department</td>
+        <td>subject</td>
+      </tr>
+    </table>
+
+    <table v-for="professor in filterBy(professors, nameFilter, 'name')">
+      <tr>
+        <td>{{ professor.name }}</td>
+        <td>{{ professor.title }}</td>
+        <td>{{ professor.school }}</td>
+        <td>{{ professor.department }}</td>
+        <td>{{ professor.subject }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Vue2Filters from "vue2-filters";
+export default {
+  mixins: [Vue2Filters.mixin],
+  data: function() {
+    return {
+      professors: [],
+      nameFilter: "",
+    };
+  },
+  created: function() {
+    axios.get("/professors").then((response) => {
+      console.log("All Professors:", response.data);
+      this.professors = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
