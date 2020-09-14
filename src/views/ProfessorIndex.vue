c7ef709ef89a9d7416c607de42c5519ffcc14e5b
Stratos-Chen <twinschen55@gmail.com>
2020-09-14 11:59:42 -0700

    added new elements to ProfessorIndex page

--- src/views/ProfessorIndex.vue
+++ src/views/ProfessorIndex.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="Professor Index">
+    <form role="search" method="get" class="search-form" action="">
+      <label>
+        <span class="screen-reader-text">Search for:</span>
+        <input type="search" v-model="nameFilter" class="search-field" />
+      </label>
+    </form>
+    <br />
+    <button class="dialog-button" v-on:click="addProfessor()">
+      Add Professor
+    </button>
+    <!-- Professor Index -->
     <table style="width:1000px">
       <tr>
         <td style="width:200px">Professor's Name</td>
@@ -12,7 +23,7 @@
 
     <table
       v-for="professor in filterBy(professors, nameFilter, 'name')"
-      style="width:1000px"
+      style="width:1100px"
     >
       <tr>
         <td style="width:200px">
@@ -24,8 +35,37 @@
         <td style="width:200px">{{ professor.school }}</td>
         <td style="width:200px">{{ professor.department }}</td>
         <td style="width:200px">{{ professor.subject }}</td>
+        <button
+          class="btn-secondary"
+          style="width:100px"
+          v-on:click="deleteProfessor(professor.id)"
+        >
+          Delete Professor
+        </button>
       </tr>
     </table>
+
+    <!-- New Professor Entry -->
+
+    <dialog id="professor-new">
+      <h2>Create New Professor</h2>
+      <form id="professor-create">
+        <label>Name:</label>
+        <input type="text" v-model="newProfessorName" />
+        <label>Title:</label>
+        <input type="text" v-model="newProfessorTitle" />
+        <label>School:</label>
+        <input type="text" v-model="newProfessorSchool" />
+        <label>Department:</label>
+        <input type="text" v-model="newProfessorDepartment" />
+        <label>Subject:</label>
+        <input type="text" v-model="newProfessorSubject" />
+
+        <button class="dialog-button" v-on:click="createProfessor()">
+          Add Professor
+        </button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -38,6 +78,17 @@ export default {
     return {
       professors: [],
       nameFilter: "",
+      newProfessorName: "",
+      newProfessorTitle: "",
+      newProfessorSchool: "",
+      newProfessorDepartment: "",
+      newProfessorSubject: "",
+      errors: [],
+      name: "",
+      title: "",
+      school: "",
+      department: "",
+      subject: "",
     };
   },
   created: function() {
@@ -46,6 +97,35 @@ export default {
       this.professors = response.data;
     });
   },
-  methods: {},
+  methods: {
+    addProfessor: function() {
+      document.querySelector("#professor-new").showModal();
+    },
+    createProfessor: function() {
+      var params = {
+        name: this.newProfessorName,
+        title: this.newProfessorTitle,
+        school: this.newProfessorSchool,
+        department: this.newProfessorDepartment,
+        subject: this.newProfessorSubject,
+      };
+      axios
+        .post("/professors", params)
+        .then((response) => {
+          console.log("Professor Created", response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    deleteProfessor: function(id) {
+      if (confirm("Are you sure you want to delete this professor?")) {
+        axios.delete(`/professors/${id}`).then((response) => {
+          console.log("Professor Deleted", response.data);
+          this.$router.push("/professors");
+        });
+      }
+    },
+  },
 };
 </script>
